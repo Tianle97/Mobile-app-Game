6d9ad54ca9bbfd808d1dc69cd86fe7690dd8f527
tianle97 <386466921@qq.com>
2018-11-05 14:38:06 +0000

    create a scene for begin with the game and create a LoadSceneOnEnter.When User Press the Enter can begin the game.That Means change the scene to next Scene to play the game

---
+++ Ghost/Assets/Scripts/LoadSceneOnEnter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnEnter : MonoBehaviour {
+
+    public string SceneNameToLoad;
+
+	// Update is called once per frame
+	void Update () {
+         if (Input.GetKeyDown(KeyCode.Return))
+        {
+          if(SceneNameToLoad != "")
+        {
+          SceneManager.LoadScene(SceneNameToLoad);
+        }
+          }
+   
+    }
+}
