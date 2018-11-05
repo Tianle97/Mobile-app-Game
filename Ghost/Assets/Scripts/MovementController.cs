a4b9db697805157e7e096dddf420fe219de52e27
tianle97 <386466921@qq.com>
2018-11-05 00:09:40 +0000

    fixed some problem in the script of MoveController.cs

--- Ghost/Assets/Scripts/MovementController.cs
+++ Ghost/Assets/Scripts/MovementController.cs
@@ -29,6 +29,7 @@ public class MovementController : MonoBehaviour {
     bool movementDisable;
 
     //Sprite Rendering
+
     bool facingRight;
     public bool IsFacingRight()
     {
@@ -66,11 +67,97 @@ public class MovementController : MonoBehaviour {
     {
         if(healthController != null)
         {
+            if(healthController.GetHealth() <= 0)
+            {
+                return;
+            }
         }
+        if(movementDisable)
+        {
+            return;
+        }
+        switch(direction)
+        {
+            case Direction.Left: rb2d.velocity = new Vector2(-moveVelocity, rb2d.velocity.y);
+                break;
+            case Direction.Right:
+                rb2d.velocity = new Vector2(moveVelocity, rb2d.velocity.y);
+                break;
+            case Direction.None:
+                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+                break;
+        }
+
+        SetSpriteDirection(direction);
     }
 
     private void SetIfInFloor()
     {
-        throw new NotImplementedException();
+        Debug.DrawLine(transform.position, (transform.position + (Vector3.down * ((spriteRenderer.bounds.size.y / 2) + 0.1f))), Color.red, 0, false);
+        RaycastHit2D floorHit = Physics2D.Raycast(transform.position, Vector2.down, (spriteRenderer.bounds.size.y / 2) + 0.1f, groundLayersMask);
+        onFloor = (floorHit.collider != null);
+        if (onFloor) performedDoubleJump = false;
+    }
+
+    public void Jump()
+    {
+        if (healthController != null)
+        {
+            if (healthController.GetHealth() <= 0)
+            {
+                return;
+            }
+        }
+
+        if (onFloor)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpVelocity);
+        }
+        else if (!performedDoubleJump)
+        {
+            performedDoubleJump = true;
+            rb2d.velocity = new Vector2(rb2d.velocity.x, doublejumpVelocity);
+        }
+    }
+    public void SetSpriteDirection(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Left:
+                if (facingRight)
+                {
+                    FlipSprite();
+                }
+                break;
+            case Direction.Right:
+                if (!facingRight)
+                {
+                    FlipSprite();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    void FlipSprite()
+    {
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+        facingRight = !facingRight;
+    }
+
+    public void Knockback(Vector3 force)
+    {
+        movementDisable = true;
+        StartCoroutine(EnableMovement(1));
+
+        // Debug.Log (force);
+        rb2d.AddForce(force);
+    }
+
+    IEnumerator EnableMovement(float time)
+    {
+        yield return new WaitForSeconds(time);
+        movementDisable = false;
     }
 }
